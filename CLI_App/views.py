9fd4dbee5c0dd9b0d55b734e8cc5450cf7190128
rkpust <rezaul.cse.pust17@gmail.com>
2024-11-17 18:37:40 +0600

    added requirements.txt

--- CLI_App/views.py
+++ CLI_App/views.py
@@ -4,24 +4,24 @@ import subprocess
 
 # Create your views here.
 def index(request):
-    input_text = 'cd'
+    # input_text = 'cd'
     if request.method == 'POST':
         input_text = request.POST.get('input_text')
         output_text = execute_command(input_text)
-        Command.objects.create(input_text=input_text, output_text=output_text)
-        currect_path = current_path_command()
+        # Command.objects.create(input_text=input_text, output_text=output_text)
+        # currect_path = current_path_command()
     else:
         input_text = ''
         output_text = ''
         
         # current_path_command()
         # Command.objects.create(input_text='cd', output_text=output_text)
-        currect_path = current_path_command()
+    currect_path = current_path_command()
 
     context = {
         'currect_path': currect_path,
         'input_text': input_text,
-        'output_text': output_text
+        'output_text': output_text,
     }
     return render(request, 'index.html', context)
 
@@ -39,5 +39,4 @@ def current_path_command():
     # For simplicity, let's just echo the command
     result = subprocess.run('cd', shell=True, capture_output=True, text=True)
     output1 = result.stdout
-    print(output1)
     return f'{output1}'
