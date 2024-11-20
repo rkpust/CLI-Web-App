f5cca7cf7bc881c42981983ca96350feb500826c
rkpust <rezaul.cse.pust17@gmail.com>
2024-11-20 17:47:06 +0600

    updated from v1.0 to v1.1

--- CLI_App/views.py
+++ CLI_App/views.py
@@ -1,36 +1,46 @@
 from django.shortcuts import render
 from .models import Command
 import subprocess
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt  # Use @csrf_exempt for simplicity in development. For production, consider using CSRF tokens.
 def index(request):
-    # input_text = 'cd'
     if request.method == 'POST':
-        input_text = request.POST.get('input_text')
-        output_text = execute_command(input_text)
-        # Command.objects.create(input_text=input_text, output_text=output_text)
-        # currect_path = current_path_command()
-    else:
-        input_text = ''
-        output_text = ''
-        
-        # current_path_command()
-        # Command.objects.create(input_text='cd', output_text=output_text)
-    currect_path = current_path_command()
+        try:
+            data = json.loads(request.body)  # Parse JSON body
+            input_text = data.get('userInput')  # Get the 'userInput' field
+            
+            # Process the input (you can add your own logic here)
+            output_text = execute_command(input_text)
 
-    context = {
-        'currect_path': currect_path,
-        'input_text': input_text,
-        'output_text': output_text,
-    }
-    return render(request, 'index.html', context)
+            return JsonResponse({'response': output_text})
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    return render(request, 'index.html')
+
 
 def execute_command(command):
     # Here you can define how to handle the command input
     # For simplicity, let's just echo the command
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     output = result.stdout
-    return f'{output}'
+    error = result.stderr
+    print(output)
+    print(result.returncode)
+
+
+    # Check if there was an error
+    if result.returncode == 0 or result.returncode == 1:
+        if output == "":
+            return f"{error}"
+        else:
+            return f"{output}"
+        # return  f"{output}" + f"{error}"
+        
 
 
 
